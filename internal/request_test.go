305df3364359208b83d6a1c8c855bc4dbf1e736f
Yasin Kinyua <yasinkinyua@gmail.com>
2022-03-01 11:19:41 +0300

    Add tests

--- internal/request_test.go
+++ internal/request_test.go
@@ -43,7 +43,38 @@ func Test_createfile(t *testing.T) {
 		t.Fatal("Coudn't create the output file")
 	}
 	outFile.Close()
+
+	if reflect.TypeOf(createfile("")) != reflect.TypeOf(outFile) {
+		t.Errorf("Value mismatch, expected file got")
+	}
 	// We just want to use this temp filename, so we delete the file,
 	// otherwise downloader creates a new file
 	os.Remove(outFile.Name())
+}
+
+func Test_cleanURl(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				url: "https://hello.com",
+			},
+			want: "https://hello.com",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cleanURl(tt.args.url); got != tt.want {
+				t.Errorf("cleanURl() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
